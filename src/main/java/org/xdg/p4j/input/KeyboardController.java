d1a798f2c3ab7d50ba376007149f9a2c9d965cf7
diego <sargent.pointed453@passinbox.com>
2026-08-07 18:02:38 +0200

    Add KeyboardController class for keyboard input handling

---
+++ src/main/java/org/xdg/p4j/input/KeyboardController.java
@@ -0,0 +1,66 @@
+package org.xdg.p4j.input;
+
+import org.xdg.p4j.data.ElementID;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyboardController extends KeyAdapter {
+
+    private final Brush brush;
+    private boolean tabPressed = false;
+    private int selectedIndex = 0;
+
+    public KeyboardController(Brush brush) {
+        this.brush = brush;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_TAB) {
+            ElementID[] elements = ElementID.values();
+            if (!tabPressed) {
+                tabPressed = true;
+                selectedIndex = brush.getCurrentElement().ordinal();
+            } else {
+                selectedIndex = (selectedIndex + 1) % elements.length;
+            }
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && tabPressed) {
+            ElementID[] elements = ElementID.values();
+            selectedIndex = (selectedIndex - 1 + elements.length) % elements.length;
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && tabPressed) {
+            ElementID[] elements = ElementID.values();
+            selectedIndex = (selectedIndex + 1) % elements.length;
+        }
+
+        if (e.getKeyCode() >= KeyEvent.VK_1 && e.getKeyCode() <= KeyEvent.VK_9) {
+            int index = e.getKeyCode() - KeyEvent.VK_1;
+            ElementID[] elements = ElementID.values();
+            if (index < elements.length) {
+                brush.setCurrentElement(elements[index]);
+                selectedIndex = index;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_TAB) {
+            tabPressed = false;
+            ElementID[] elements = ElementID.values();
+            brush.setCurrentElement(elements[selectedIndex]);
+        }
+    }
+
+    public boolean isTabPressed() {
+        return tabPressed;
+    }
+
+    public int getSelectedIndex() {
+        return selectedIndex;
+    }
+}
