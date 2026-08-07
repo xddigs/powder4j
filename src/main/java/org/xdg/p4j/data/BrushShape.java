9c60ced0bf85a044d0a54a24e4ac1b242d8cb5cd
diego <sargent.pointed453@passinbox.com>
2026-08-07 22:43:14 +0200

    Add different Brush shapes

---
+++ src/main/java/org/xdg/p4j/data/BrushShape.java
@@ -0,0 +1,23 @@
+package org.xdg.p4j.data;
+
+public enum BrushShape {
+    CIRCLE("Circle", "●"),
+    SQUARE("Square", "■"),
+    TRIANGLE("Triangle", "▲");
+
+    private final String name;
+    private final String symbol;
+
+    BrushShape(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
