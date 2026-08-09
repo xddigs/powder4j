3e3fd90b73786c48bc650c557f7e8b8830906231
diego <sargent.pointed453@passinbox.com>
2026-08-09 19:08:33 +0200

    Fix element IDs and chemical formulas

--- src/main/java/org/p4j/data/ElementID.java
+++ src/main/java/org/p4j/data/ElementID.java
@@ -6,43 +6,44 @@ package org.p4j.data;
  * Compounds, organic materials, and mixtures must be crafted or generated via reactions.
  */
 public enum ElementID {
-    EMPTY((byte) 0, "Air", "O2", 0xFF0B0E14, false, 0, false, false, false, false, false, 0),
+    EMPTY((byte) 0, "Air", "N2+O2", 0xFF0B0E14, false, 0, false, false, false, false, false, 0),
     HYDROGEN((byte) 1, "Hydrogen", "H2", 0x88E0F2FE, true, -3, true, false, false, false, false, 0),
-    SMOKE_DARK((byte) 2, "Dark Smoke", "C", 0xFF3E4451, false, -1, false, false, false, false, false, 0),
+    CARBON_DIOXIDE((byte) 2, "Carbon Dioxide", "CO2", 0xAA7F8C8D, false, -1, false, false, false, false, false, 0),
     SODIUM((byte) 3, "Sodium", "Na", 0xFFD1D5DB, true, 3, false, true, false, false, false, 0),
     CHLORINE((byte) 4, "Chlorine", "Cl2", 0xFF88FF00, true, 1, false, false, false, false, true, 4),
-    MERCURY((byte) 5, "Mercury", "Hg", 0xFFCFD8DC, true, 5, false, false, false, false, true, 1),
+    MERCURY((byte) 5, "Mercury", "Hg", 0xFFCFD8DC, false, 5, false, false, false, false, true, 1),
     WATER((byte) 6, "Water", "H2O", 0xFF4FA6ED, false, 2, false, false, true, false, true, 5),
     ICE((byte) 7, "Ice", "H2O(s)", 0xFFA5F2F3, false, 4, false, true, false, false, false, 0),
     STEAM((byte) 8, "Steam", "H2O(g)", 0xCCFFFFFF, false, -1, false, false, false, false, false, 0),
-    SMOKE_LIGHT((byte) 9, "Light Smoke", "H2O(g)", 0xFFDCDFE4, false, -1, false, false, false, false, false, 0),
-    SMOKE_GRAY((byte) 10, "Smoke", "CO2", 0xFFABB2BF, false, -1, false, false, false, false, false, 0),
+    CARBON_MONOXIDE((byte) 9, "Carbon Monoxide", "CO", 0x88555555, false, -2, true, false, false, false, false, 0),
+    ASH((byte) 10, "Ash", "C/Ash", 0xFF5C5C5C, false, 2, false, false, false, false, false, 0),
     ACID((byte) 11, "Acid", "HCl", 0xFF2ECC71, false, 2, false, false, false, false, true, 5),
     SALT((byte) 12, "Salt", "NaCl", 0xFFF5F5F5, false, 3, false, false, false, false, false, 0),
     SAND((byte) 13, "Sand", "SiO2", 0xFFE5C07B, false, 3, false, true, false, false, false, 0),
     GLASS((byte) 14, "Glass", "SiO2", 0x80E0F7FA, false, 5, false, false, false, false, false, 0),
-    LAVA((byte) 15, "Lava", "SiO2(l))", 0xFFFF4500, false, 3, false, false, false, true, true, 2),
-    OBSIDIAN((byte) 16, "Obsidian", "SiO2+", 0xFF1C1326, false, 5, false, false, false, false, false, 0),
+    LAVA((byte) 15, "Lava", "SiO2(l)", 0xFFFF4500, false, 3, false, false, false, true, true, 2),
+    OBSIDIAN((byte) 16, "Obsidian", "SiO2", 0xFF1C1326, false, 5, false, false, false, false, false, 0),
     CEMENT((byte) 17, "Cement", "CaCO3", 0xFF9E9E9E, false, 4, false, false, false, false, false, 0),
     METHANE((byte) 18, "Methane", "CH4", 0x8800FFAA, false, -2, true, false, false, false, false, 0),
     TNT((byte) 19, "TNT", "C7H5N3O6", 0xFFC23616, false, 1, true, true, false, false, false, 0),
     GASOLINE((byte) 20, "Gasoline", "C8H18", 0xFFD4A373, false, 1, true, true, false, false, true, 5),
-    OIL((byte) 21, "Oil", "CnHm", 0xFF8A9A20, false, 1, true, true, false, false, true, 4),
+    OIL((byte) 21, "Olive Oil", "C18H34O2", 0xFF8A9A20, false, 1, true, true, false, false, true, 4),
     WOOD((byte) 22, "Wood", "C6H10O5", 0xFF8B5A2B, false, 4, true, true, false, false, false, 0),
     MUD((byte) 23, "Mud", "SiO2+H2O", 0xFF3E2723, false, 4, true, true, false, false, true, 1),
     SEED((byte) 24, "Seed", "Sd", 0xFF8BC34A, true, 3, true, true, false, false, false, 0),
-    THERMITE((byte) 25, "Thermite", "Fe+Al", 0xFFB71C1C, false, 4, true, false, false, false, false, 0),
-    GUNPOWDER((byte) 26, "Gunpowder", "KNO3", 0xFF53565A, false, 3, true, true, false, false, false, 0),
-    STONE((byte) 27, "Stone", "ST", 0xFF808080, false, 4, false, false, false, false, false, 0),
-    GRAVEL((byte) 28, "Gravel", "Gr", 0xFF9B8773, false, 3, false, true, false, false, false, 0),
-    DIRT((byte) 29, "Dirt", "Soil", 0xFF5D4037, true, 3, false, true, false, false, false, 0),
-    WET_SAND((byte) 30, "Wet Sand", "SiO2", 0xFF9E753B, false, 4, false, true, false, false, false, 0),
+    THERMITE((byte) 25, "Thermite", "Fe2O3+Al", 0xFFB71C1C, false, 4, true, false, false, false, false, 0),
+    GUNPOWDER((byte) 26, "Gunpowder", "KNO3+S+C", 0xFF53565A, false, 3, true, true, false, false, false, 0),
+    STONE((byte) 27, "Stone", "SiO2", 0xFF808080, false, 4, false, false, false, false, false, 0),
+    GRAVEL((byte) 28, "Gravel", "SiO2", 0xFF9B8773, false, 3, false, true, false, false, false, 0),
+    DIRT((byte) 29, "Dirt", "Soil", 0xFF5D4037, false, 3, false, true, false, false, false, 0),
+    WET_SAND((byte) 30, "Wet Sand", "SiO2+H2O", 0xFF9E753B, false, 4, false, true, false, false, false, 0),
     FIRE((byte) 31, "Fire", "Q", 0xFFFF5722, true, 0, false, false, false, true, false, 0),
-    GRASS((byte) 32, "Grass", "G", 0xFF4CAF50, false, 4, true, true, false, false, false, 0),
+    GRASS((byte) 32, "Grass", "C6H10O5", 0xFF4CAF50, false, 4, true, true, false, false, false, 0),
     SILICON((byte) 33, "Silicon", "Si", 0xFF5C6BC0, true, 4, false, true, false, false, false, 0),
-    CARBON((byte) 34, "Carbon", "C", 0xFF222222, false, 3, true, false, false, false, false, 0),
+    CARBON((byte) 34, "Carbon", "C", 0xFF222222, true, 3, true, false, false, false, false, 0),
     OXYGEN((byte) 35, "Oxygen", "O2", 0x88B0E0E6, true, -2, false, false, false, false, false, 3),
-    IRON((byte) 36, "Iron", "Fe", 0xFF795548, true, 4, false, false, false, true, false, 0);
+    IRON((byte) 36, "Iron", "Fe", 0xFF795548, true, 4, false, false, false, true, false, 0),
+    NITROGEN((byte) 37, "Nitrogen", "N2", 0x88A0C4FF, true, -2, false, false, false, false, false, 0);
 
     private final byte id;
     private final String name;
@@ -143,7 +144,7 @@ public enum ElementID {
         return this == SAND || this == SILICON || this == DIRT ||
                 this == SEED || this == SALT || this == SODIUM ||
                 this == CEMENT || this == GUNPOWDER || this == THERMITE ||
-                this == CARBON;
+                this == CARBON || this == ASH;
     }
 
     public boolean isSolid() {
