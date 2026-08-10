8709dab3a642061af5b9e3daacff3cb149a99703
diego <sargent.pointed453@passinbox.com>
2026-08-10 12:40:05 +0200

    Add more data fields to ElementCard

--- src/main/java/org/p4j/data/ElementCard.java
+++ src/main/java/org/p4j/data/ElementCard.java
@@ -1,5 +1,11 @@
 package org.p4j.data;
 
-public record ElementCard(ElementID e, byte id,
-                          String name, String formula,
-                          int temperature) {}
+public record ElementCard(
+        ElementID element,
+        byte id,
+        String name,
+        String symbol,
+        float liveTemp,
+        float defaultTemp,
+        float boilingPoint
+) {}
