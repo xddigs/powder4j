80327ecb39fe785d2267540f9e2c68ac734f5843
diego <sargent.pointed453@passinbox.com>
2026-08-10 16:57:09 +0200

    Update version >> 1.0.0

--- src/main/java/org/p4j/core/K.java
+++ src/main/java/org/p4j/core/K.java
@@ -11,7 +11,7 @@ public final class K {
     private K() {}
 
     public static final String APP_TITLE = "Powder4J ";
-    public static final String VERSION = "0.6.0";
+    public static final String VERSION = "1.0.0";
     public static final Font FONT_BIG = Fonts.get(32f, Font.PLAIN);
     public static final Font FONT_SMALL = Fonts.get(18f, Font.PLAIN);
     public static final Font FONT_TINY = Fonts.get(16f, Font.PLAIN);
