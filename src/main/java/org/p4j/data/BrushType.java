1278fb716b015da76c0d9dff22e0286f9e7cf0e6
diego <sargent.pointed453@passinbox.com>
2026-08-11 19:18:14 +0200

    Add Dropper to the list of tools

--- src/main/java/org/p4j/data/BrushType.java
+++ src/main/java/org/p4j/data/BrushType.java
@@ -3,7 +3,8 @@ package org.p4j.data;
 public enum BrushType {
     BRUSH("Brush", "\uF1FC"),
     FILLER("Filler", "\uF043"),
-    ERASER("Eraser", "\uF12D");
+    ERASER("Eraser", "\uF12D"),
+    DROPPER("Dropper", "\uF1FB");
 
     private final String name;
     private final String symbol;
